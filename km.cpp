5020d4353c717c9901e5ea94320b7186a4bc3fca
ArtSv <ArtSv@DESKTOP-JK44D29>
2024-05-13 10:31:48 +0300

    Добавьте файлы проекта.

---
+++ km.cpp
@@ -0,0 +1,23 @@
+﻿// km.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    
+    float m, km;
+
+    cout << "Введите метры: ";
+    cin >> m;
+
+    km = m / 1000;
+    cout << m << " метров будет " << km << " километра" << endl;
+
+    return 0;
+}
+
+
